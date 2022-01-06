d467046f9ef38af22576264deef556e071e11ea0
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-01-06 22:40:20 +0530

    initial commit

---
+++ Maths/B. Margarite and the best present.cpp
@@ -0,0 +1,105 @@
+//Problem Link - https://codeforces.com/contest/1080/problem/B
+/* Pratyaksh Khurana */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define ff first
+#define ss second
+ 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+/*******************************************************************************************************************************************************************************************************/
+
+
+int Odd(int L, int R){
+    int N = (R - L) / 2;
+    if (R % 2 != 0 || L % 2 != 0)
+        N += 1;
+    return N;
+}
+
+void solve(){
+	int t; cin>>t;
+
+	while(t--){
+		int l,r; cin>>l>>r;
+		if(l == r){
+			if( l % 2 == 0){
+				cout<<l<<endl;
+			}else{
+				cout<<(-1) * l<<endl;
+			}
+			continue;
+		}
+
+		ll odd = Odd(l,r);
+		ll even = (r - l + 1) - odd,ev = 0,od = 0;
+	
+		if(l % 2  == 0 && r % 2 == 0){
+			ev = even * (l + r) / 2;
+			od = odd * (l + r) / 2;
+		
+		}else if(l % 2  == 0 && r % 2 != 0){
+			ev = even * (l + r - 1) / 2;
+			od = odd * (l + 1 + r) / 2;
+		}else if(l % 2  != 0 && r % 2 != 0){
+			ev = even * (l + r) / 2;
+			od = odd * (l + r) / 2;
+		}else if(l % 2  != 0 && r % 2 == 0){
+			ev = even * (l + 1 + r) / 2;
+			od = odd * (l + r - 1) / 2;
+		}
+
+		cout<<ev - od<<endl;
+
+	}
+	
+}
+int main() {
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	  solve();
+	return 0;
+}
